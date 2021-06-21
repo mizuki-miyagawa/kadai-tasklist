8ed5e39239beb6b9e22845cf18755740b0f7c798
miyagawa.mizuki <wakimizu3333@gmail.com>
2021-06-21 13:43:22 +0000

    login

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_132602) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
