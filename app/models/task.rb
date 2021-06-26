802131153c6c841e4755ad3093dba993dc764546
miyagawa.mizuki <wakimizu3333@gmail.com>
2021-06-26 07:56:29 +0000

    update

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,4 @@ class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 225 }
   validates :status, presence: true, length: { maximum: 10 }
   belongs_to :user
-  
-  validates :content, presence: true, length: { maximum: 255 }
 end
--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-(1..100).each do |number|
-  Task.create(content: 'test content ' + number.to_s, status: 'test status' + number.to_s)
-end
